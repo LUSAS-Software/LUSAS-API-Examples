5f3cf22b15c1fca354bc5efeb890efc58dd12eee
Apostolos Grammatopoulos <greatapo@gmail.com>
2025-04-23 12:13:34 +0100

    Added new model script

---
+++ python/01b_Create_New_Model.py
@@ -0,0 +1,32 @@
+# LUSAS API (LPI) PYTHON EXAMPLES
+# (https://github.com/LUSAS-Software/LUSAS-API-Examples/)
+#
+# Example:      01b_Create_New_Model.py
+# Description:  Create a new LUSAS model
+# 
+
+# Libraries:
+# LUSAS LPI module (easier connection and autocomplete)
+from shared.LPI_21_1 import *
+
+# Connect on LUSAS and check if a model is open
+lusas = get_lusas_modeller()
+
+# Check if a model is open and not saved
+if lusas.existsDatabase() and lusas.db().isModified():
+    raise Exception("Save or close the current model before running this code")
+
+
+# Create a new model
+filename = "my_new_model.mdl"
+lusas.newProject("Structural", filename)
+
+# Get a reference to the model database
+db = lusas.getDatabase()
+
+# Set the analysis category & vertical axis
+db.setAnalysisCategory("3D")
+db.setVerticalDir("Z")
+
+# Set the unit system
+db.setModelUnits("kN,m,kg,s,C")
